05081518cd29c8a377b0fcae9eefd92d2a4059ea
Igor Kolomeicev <IgorKolomeicev@yandex.ru>
2022-04-20 21:42:26 +0300

    fix review

--- javascript/index.js
+++
@@ -1,7 +0,0 @@
-const mainScript = document.createElement('script');
-mainScript.src = './javascript/main.js';
-document.head.appendChild(mainScript)
-
-const popupScript = document.createElement('script');
-popupScript.src = './javascript/popup.js';
-document.head.appendChild(popupScript);
--- script/index.js
+++ script/index.js
@@ -1,11 +1,18 @@
+const editButton = document.querySelector('.profile__edit-button');
+const profileName = document.querySelector('.profile__title');
+const profileAbout = document.querySelector('.profile__subtitle');
 const closeButtom = document.querySelector('.popup__close-button');
 const popup = document.querySelector('.popup');
 const popupName = document.querySelector('.popup__name');
 const popupAbout = document.querySelector('.popup__about');
 const popupForm = document.querySelector('.popup__form');
 
-popupName.value = window.profileName.innerHTML;
-popupAbout.value = window.profileAbout.innerHTML;
+popupName.value = profileName.innerHTML;
+popupAbout.value = profileAbout.innerHTML;
+
+editButton.addEventListener('click', () => {
+    popup.classList.add('popup_opened');
+});
 
 closeButtom.addEventListener('click', () => {
     popup.classList.remove('popup_opened');
@@ -13,9 +20,7 @@ closeButtom.addEventListener('click', () => {
 
 popupForm.addEventListener('submit', (event) => {
     event.preventDefault();
-    window.profileName.innerHTML = event.target.name.value;
-    window.profileAbout.innerHTML = event.target.about.value;
+    profileName.innerHTML = event.target.name.value;
+    profileAbout.innerHTML = event.target.about.value;
     popup.classList.remove('popup_opened');
 });
-
-window.popup = popup;
