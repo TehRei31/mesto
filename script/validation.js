fd75bb90714f2e02158d1a570b8189e19671f559
Igor Kolomeicev <IgorKolomeicev@yandex.ru>
2022-08-23 00:57:20 +0300

    element validation

--- script/validation.js
+++ script/validation.js
@@ -1,64 +1,124 @@
-// ADD PLACE VALIDATION
-const profileForm = document.querySelector('.popup__form_type_edit-profile');
-const submitProfileForm = profileForm.querySelector('.popup__submit')
-const profileFormErrors = {
-    name: '',
-    about: ''
-};
+function formValidateListener(validateFn, submitButton) {
+    return (event) => {
+        const {target} = event;
 
-const MIN_NAME = 2;
-const MAX_NAME = 40;
-function validateProfileName(value) {
-    let error = '';
+        const hasErrorForm = validateFn(target);
 
-    if (value.length === 0) {
-        error = 'Вы пропустили это поле';
-    } else if (value.length < MIN_NAME) {
-        error = `Минимальное количество символов: ${MIN_NAME}. Длина текста сейчас: ${value.length}`;
-    } else if (value.length > MAX_NAME) {
-        error = `Максимальное количество символов: ${MAX_NAME}. Длина текста сейчас: ${value.length}`;
-    }
-
-    return error;
+        if (hasErrorForm) {
+            submitButton.classList.add('popup__submit_disabled');
+        } else {
+            submitButton.classList.remove('popup__submit_disabled');
+        }
+    };
 }
 
-const MIN_ABOUT = 2;
-const MAX_ABOUT = 200;
-function validateProfileAbout(value) {
+function setInputError(target, error) {
+    const inputError = target.nextElementSibling;
+    inputError.innerHTML = error;
+
+    if (error) {
+        target.classList.add('popup__input_error');
+    } else {
+        target.classList.remove('popup__input_error');
+    }
+}
+
+function validateTextInput(value, min, max) {
     let error = '';
 
     if (value.length === 0) {
         error = 'Вы пропустили это поле.';
-    } else if (value.length < MIN_ABOUT) {
-        error = `Минимальное количество символов: ${MIN_ABOUT}. Длина текста сейчас: ${value.length}.`;
-    } else if (value.length > MAX_ABOUT) {
-        error = `Максимальное количество символов: ${MAX_ABOUT}. Длина текста сейчас: ${value.length}.`;
+    } else if (value.length < min) {
+        error = `Минимальное количество символов: ${min}. Длина текста сейчас: ${value.length}.`;
+    } else if (value.length > max) {
+        error = `Максимальное количество символов: ${max}. Длина текста сейчас: ${value.length}.`;
     }
 
     return error;
 }
 
-profileForm.addEventListener('input', (event) => {
-    const {target} = event;
-    const inputError = target.nextElementSibling;
 
+// PROFILE VALIDATION
+const profileForm = document.querySelector('.popup__form_type_edit-profile');
+const submitProfileForm = profileForm.querySelector('.popup__submit')
+
+const MIN_PROFILE_NAME = 2;
+const MAX_PROFILE_NAME = 40;
+function validateProfileName(value) {
+    return validateTextInput(value, MIN_PROFILE_NAME, MAX_PROFILE_ABOUT);
+}
+
+const MIN_PROFILE_ABOUT = 2;
+const MAX_PROFILE_ABOUT = 200;
+function validateProfileAbout(value) {
+    return validateTextInput(value, MIN_PROFILE_ABOUT, MAX_PROFILE_ABOUT);
+}
+
+const profileFormErrors = {
+    name: '',
+    about: ''
+};
+function validateProfile(target) {    
     if (target.name === 'name') {
         const error = validateProfileName(target.value);
-        inputError.innerHTML = error;
         profileFormErrors.name = error;
-        if (error) {
-            target.classList.add('popup__input_error');
-        } else {
-            target.classList.remove('popup__input_error');
-        }
+        setInputError(target, error);
+        
     } else if (target.name === 'about') {
-        profileFormErrors.about = validateProfileAbout(target.value);
-        inputError.innerHTML = profileFormErrors.about;
+        const error = validateProfileAbout(target.value);
+        profileFormErrors.about = error;
+        setInputError(target, error);
     }
 
-    if (profileFormErrors.name || profileFormErrors.about) {
-        submitProfileForm.classList.add('popup__submit_disabled');
+    return profileFormErrors.name || profileFormErrors.about;
+}
+
+profileForm.addEventListener('input', formValidateListener(validateProfile, submitProfileForm));
+
+
+// ADD ELEMENT VALIDATION
+const addElementForm = document.querySelector('.popup__form_type_add-element');
+const submitElementForm = addElementForm.querySelector('.popup__submit')
+
+const MIN_ELEMENT_NAME = 1;
+const MAX_ELEMENT_NAME = 30;
+function validateElementName(value) {
+    return validateTextInput(value, MIN_ELEMENT_NAME, MAX_ELEMENT_NAME);
+}
+
+function validateElementLink(value) {
+    let error = '';
+
+    if (value.length === 0) {
+        error = 'Вы пропустили это поле.';
     } else {
-        submitProfileForm.classList.remove('popup__submit_disabled');
+        try {
+            new URL(value);
+        } catch (e) {
+            error = 'Введите адрес сайта.';
+        }
     }
-});
+
+    return error;
+}
+
+const elementFormErros = {
+    name: '',
+    link: ''
+};
+function validateElement(target) {    
+    if (target.name === 'name') {
+        const error = validateElementName(target.value);
+        elementFormErros.name = error;
+        setInputError(target, error);
+        
+    } else if (target.name === 'link') {
+        const error = validateElementLink(target.value);
+        elementFormErros.link = error;
+        setInputError(target, error);
+    }
+
+    return elementFormErros.name || elementFormErros.link;
+}
+
+addElementForm.addEventListener('input', formValidateListener(validateElement, submitElementForm));
